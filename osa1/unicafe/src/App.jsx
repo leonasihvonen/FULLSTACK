1dffa680307fe361c58d8ec9f928d4ee99564f18
leonasihvonen <leonasih@uef.fi>
2024-01-02 14:34:09 +0200

    1.7 unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -11,17 +11,49 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [averageValues, setAverageValues] = useState(0)
+  const [calc, setCalc] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    const updatedAverageValues = averageValues + 1
+    setAverageValues(updatedAverageValues)
+    const updatedCalc = calc + 1
+    setCalc(updatedCalc)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+    setAverage(updatedAverageValues/updatedCalc)
+    const updatedPositive = updatedGood / (updatedGood+neutral+bad) * 100
+    setPositive(updatedPositive)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    const updatedAverageValues = averageValues + 0
+    setAverageValues(updatedAverageValues)
+    const updatedCalc = calc + 1
+    setCalc(updatedCalc)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+    setAverage(updatedAverageValues/updatedCalc)
+    const updatedPositive = good / (good+updatedNeutral+bad) * 100
+    setPositive(updatedPositive)
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
+    const updatedAverageValues = averageValues - 1
+    setAverageValues(updatedAverageValues)
+    const updatedCalc = calc + 1
+    setCalc(updatedCalc)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+    setAverage(updatedAverageValues/updatedCalc)
+    const updatedPositive = good / (good+neutral+updatedBad) * 100
+    setPositive(updatedPositive)
   }
 
   return (
@@ -34,7 +66,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
-
+      <p>All {total}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive + '%'}</p>
     </div>
   )
 }

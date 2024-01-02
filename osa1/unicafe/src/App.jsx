cf2f03fe4298674336e1fbee755f0efb2b5f70a9
leonasihvonen <leonasih@uef.fi>
2024-01-02 13:09:39 +0200

    1.6

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <h1>{props.header}</h1>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header header="Give feedback"/>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <Header header="Statistics"/>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+
     </div>
   )
 }
